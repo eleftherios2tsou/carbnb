9e9cc9bfd14a526ca53078a1227177bfa844b927
Lefteris <lefteris02tsourdiou@gmail.com>
2025-08-17 12:06:45 +0300

    Added Database URL to settings and env

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import AnyUrl
 class Settings(BaseSettings):
     app_env: str = "dev"
-
+    database_url: AnyUrl | str = "postgresql+psycopg://postgres:postgres@localhost:5432/carbnb"
     class Config:
         env_file = ".env"
         case_sensitive = False
